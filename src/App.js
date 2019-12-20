e66ddd48b28a562a54d330d8c3d6cb06393a45b6
skooger <adamjskooglambda@gmail.com>
2019-12-20 09:13:18 -0800

    Completed retrieving the character list and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Switch>
+        <Route exact path="/">
+          <WelcomePage/>
+        </Route>
+        <Route path="/characters">
+          <CharacterList/>
+        </Route>
+
+      </Switch>
+      
+      
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters,setCharacters] = useState([]);
+  const axios = require('axios').default;
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const fetchData = async () => {
+      const response = await axios.get('https://rickandmortyapi.com/api/character/');
+      console.log(response.data.results);
+      setCharacters(response.data.results);
+    }
+    fetchData();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+     {characters.map((char,index) => (
+        <CharacterCard key={index}
+                      char={char}/>
+
+      ))}
     </section>
   );
 }
